d0455ce80da77bd8e7ef131454b54e3af0719533
rajitaaa <rajitag31@gmail.com>
2016-10-26 00:43:26 +0530

    Create linearsearch.cpp

---
+++ Searching_Algorithms/Linear_Search/linearsearch.cpp
@@ -0,0 +1,39 @@
+#include<conio.h>
+#include<iostream.h>
+void main()
+{
+clrscr();
+
+int a[100],i,n,item,s=0;
+
+
+cout<<"Enter No. of Elements=";
+cin>>n;
+
+cout<<"\nEnter Elements=\n";
+for(i=1;i<=n;i++)
+{
+cin>>a[i];
+}
+
+cout<<"\nEnter Element you want to Search=";
+cin>>item;
+
+for(i=1;i<=n;i++)                       
+{
+if(a[i]==item)
+{
+cout<<"\nData is Found at Location : "<<i;
+s=1;
+break;
+}
+}
+
+if(s==0)
+{
+cout<<"Data is Not Found";
+}
+
+
+getch();
+}
